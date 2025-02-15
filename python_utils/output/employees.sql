3cdd4920d4977183179bd12b3f7352bb568f093c
Chong Lee <upgamesemail@gmail.com>
2025-02-15 02:55:42 +0200

    generated the new things

---
+++ python_utils/output/employees.sql
@@ -0,0 +1,19 @@
+CREATE TABLE EMPLOYEE(
+    EmployeeID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(64) NOT NULL,
+    LastName VARCHAR(64) NOT NULL,
+    PhoneNr VARCHAR(16) NOT NULL
+);
+
+INSERT INTO EMPLOYEE(FirstName, LastName, PhoneNr)
+VALUES
+    ("lugij", "dipuneha", "+358100000032"),
+    ("sene", "rivape", "+358100000033"),
+    ("ziteze", "qupa", "+358100000034"),
+    ("cuweroj", "cove", "+358100000035"),
+    ("vyka", "tosu", "+358100000036"),
+    ("cumusagi", "satedize", "+358100000037"),
+    ("fybata", "nuveg", "+358100000038"),
+    ("juhoma", "cyba", "+358100000039"),
+    ("vabiro", "qamuxunu", "+358100000040"),
+    ("dozylez", "hylaku", "+358100000041");
