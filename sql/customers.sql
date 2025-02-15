bab7925b54d2884df43f98b7be7c53253d25f5a0
Chong Lee <upgamesemail@gmail.com>
2025-02-15 06:08:10 +0200

    applied changes

---
+++ sql/customers.sql
@@ -0,0 +1,39 @@
+CREATE TABLE CUSTOMER(
+    EmployeeID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(64) NOT NULL,
+    LastName VARCHAR(64) NOT NULL,
+    PhoneNr VARCHAR(16) NOT NULL
+);
+
+INSERT INTO CUSTOMER(FirstName, LastName, PhoneNr)
+VALUES
+    ("revelolo", "nujeluje", "+358100000042"),
+    ("copekot", "xuvy", "+358100000043"),
+    ("qylarex", "heqitu", "+358100000044"),
+    ("xuqef", "kyfocesy", "+358100000045"),
+    ("rugawify", "vexiga", "+358100000046"),
+    ("jyjetehy", "wonu", "+358100000047"),
+    ("rihyla", "borovipi", "+358100000048"),
+    ("zyse", "mifoz", "+358100000049"),
+    ("mukar", "darego", "+358100000050"),
+    ("wahe", "zavosi", "+358100000051"),
+    ("marafagy", "hadinuw", "+358100000052"),
+    ("jizon", "soruc", "+358100000053"),
+    ("boky", "jihivumi", "+358100000054"),
+    ("sumy", "qamyguqa", "+358100000055"),
+    ("capigidu", "fiqetut", "+358100000056"),
+    ("kyze", "ropil", "+358100000057"),
+    ("luwidopo", "zipahote", "+358100000058"),
+    ("roma", "piry", "+358100000059"),
+    ("subyru", "vyqi", "+358100000060"),
+    ("figu", "qigi", "+358100000061"),
+    ("dyfisy", "sidule", "+358100000062"),
+    ("mytagisy", "pebote", "+358100000063"),
+    ("gemo", "viqul", "+358100000064"),
+    ("hyjeh", "tirefoce", "+358100000065"),
+    ("tymid", "qenogin", "+358100000066"),
+    ("fokoby", "luza", "+358100000067"),
+    ("midulysi", "zujyj", "+358100000068"),
+    ("jyfibado", "vavi", "+358100000069"),
+    ("xefu", "vubij", "+358100000070"),
+    ("tydezyhe", "gakyviq", "+358100000071");
--- sql/hotels.sql
+++ sql/hotels.sql
@@ -7,5 +7,5 @@ CREATE TABLE HOTEL(
 
 INSERT INTO HOTEL(HotelName, Address, FrontDeskNR)
 VALUES
-    ("Trivago", "Tikkuraitinkuja 2, Vantaa 01350", "+358100000000"),
+    ("Trivago", "Joulukylänkuja 2, Korvatunturi 80010", "+358100000000"),
     ("Korvatunturi", "Korvatunturintie 1, Korvatunturi 80000", "+358100000001");
