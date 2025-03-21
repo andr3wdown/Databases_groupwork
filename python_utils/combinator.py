b2fcb9d95caa61e0bb8040eeaafce83e0d4be74c
andr3wdown <upgamesemail@gmail.com>
2025-03-21 08:57:23 +0200

    created a combined sql file for creating the whole database

---
+++ python_utils/combinator.py
@@ -0,0 +1,16 @@
+import os
+
+
+def combine_files(path):
+    files = ["hotels.sql", "rooms.sql", "customers.sql", "employees.sql", "reservations.sql", "shifts.sql"]
+    files = [os.path.join(path, f) for f in files]
+    files.sort(key=os.path.getctime)  # Sort files by creation time
+    with open(os.path.join(path, 'combined.sql'), 'w') as outfile:
+        for fname in files:
+            with open(fname) as infile:
+                outfile.write('\n')
+                outfile.write(infile.read())
+                outfile.write('\n')
+
+if __name__ == '__main__':
+    combine_files('../creation')
